ab2b90357df4e730d13e551a0431b48711909ad5
Matvey Kommercheskiy <kommercheskiy.m@wb.ru>
2023-12-14 16:49:28 +0300

    add logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,70 @@
+'use strict'
 
-function submitForm() {
-    const inputText = document.querySelector('.input').value;
-    document.querySelector('.panel').innerText = inputText;
-    document.querySelector('.input').value = '';
-    document.querySelector('.notification').classList.add('notification_active');
-};
-function changeTextButtonClicked() {
-    submitForm();
-};
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
 
-function inputChanged(e) {
-    if (e.code == 'Enter') {
-        submitForm();
+/* page */
+
+const page = {
+    menu: document.querySelector('.menu__list')
+}
+
+/* utils */
+
+function loadData() {
+    const habbitStrings = localStorage.getItem(HABBIT_KEY);
+    const habbitsArray = JSON.parse(habbitStrings);
+
+    if (Array.isArray(habbitsArray), habbitsArray.length) {
+        habbits = habbitsArray;
     }
-};
+}
 
+function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+/* render */
+
+function rerenderMenu(activeHabbit) {
+    if (!activeHabbit) {
+        return;
+    }
+
+    for (const habbit of habbits) {
+        const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+        if (!existed) {
+            const element = document.createElement('button');
+            element.setAttribute('menu-habbit-id', habbit.id);
+            element.classList.add('menu__item');
+            element.addEventListener('click', () => rerender(habbit.id));
+            element.innerHTML = `<img src="./images/${habbit.icon}.svg" alt="${habbit.name}" />`;
+
+            if (activeHabbit.id === habbit.id) {
+                element.classList.add('menu__item__active');
+            }
+            page.menu.appendChild(element);
+            continue;
+        }
+
+        if (activeHabbit.id === habbit.id ) {
+            existed.classList.add('menu__item__active');
+        } else {
+            existed.classList.remove('menu__item__active');
+        }
+    }
+}
+
+function rerender(activeHabbitId) {
+    const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
+    rerenderMenu(activeHabbit);
+}
+
+/* init */
+
+(() => {
+    loadData();
+    if (habbits.length) {
+        rerender(habbits[0].id)
+    }
+})();
